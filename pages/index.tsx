dc311b3f6393450c76eb11dfa275d18e13f37790
Michele Baruffetti <michelebaruffetti@gmail.com>
2022-09-23 11:36:17 +0200

    [feat] rxjs all pokemon call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { getAllPokemon } from "../queries/pokemon";
 
-const Home: NextPage = () => {
+const Home: NextPage<{ allPokemon: any }> = ({ allPokemon }) => {
+  console.log({ allPokemon });
+
   return <>test</>;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await getAllPokemon();
+
+  return { props: { allPokemon: res } };
 };
 
 export default Home;
---
+++ queries/pokemon.ts
@@ -0,0 +1,43 @@
+import {
+  catchError,
+  forkJoin,
+  from,
+  lastValueFrom,
+  mergeMap,
+  throwError,
+} from "rxjs";
+
+const baseUrlPokemon = "https://pokeapi.co/api/v2/";
+const request = (url: string) => from(fetch(url).then((res) => res.json()));
+
+export const getAllPokemonUrl = () => {
+  const url = baseUrlPokemon + "pokemon?limit=100000&offset=0";
+
+  const obs$ = from(fetch(url)).pipe(
+    mergeMap((response: Response) => {
+      return response.json();
+    }),
+    catchError((error: Error) => {
+      return throwError(() => new Error(error.message));
+    })
+  );
+
+  return lastValueFrom(obs$);
+};
+
+export const getAllPokemon = async () => {
+  const pokemonUrl = from(getAllPokemonUrl());
+
+  const obs$ = from(
+    pokemonUrl.pipe(
+      mergeMap((response) =>
+        forkJoin(response.results.map((el: any) => request(el.url)))
+      ),
+      catchError((error: Error) => {
+        return throwError(() => new Error(error.message));
+      })
+    )
+  );
+
+  return lastValueFrom(obs$);
+};
