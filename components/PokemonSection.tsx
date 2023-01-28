8d2f408f07e94d6bac9b56043175b4de1017a255
Michele Baruffetti <michelebaruffetti@gmail.com>
2023-01-28 18:57:59 +0100

    [fix]all cards same height, kind of a Deck Card but responsive all breakpoints

--- components/PokemonSection.tsx
+++ components/PokemonSection.tsx
@@ -1,19 +1,34 @@
 import { FC } from "react";
+import { Button, Card, CardGroup, Col, Container, Row } from "react-bootstrap";
 
 const PokemonSection: FC<{ allPokemon: any }> = ({ allPokemon }) => {
   return (
-    <>
-      {/* <Card.Group>
+    <Container>
+      <Row xs={1} md={2} lg={3} xl={4} xxl={6} className="g-2">
         {allPokemon.map((el: any) => (
-          <Card key={el?.id}>
-            <Image src={el?.sprites.other.dream_world.front_default} />
-            <Card.Content>
-              <Card.Header>{el?.name?.toUpperCase()}</Card.Header>
-            </Card.Content>
-          </Card>
+          <Col className="d-flex">
+            <Card
+              //   style={{ width: "18rem" }}
+              key={el?.id}
+              className="flex-fill"
+            >
+              <Card.Img
+                variant="top"
+                src={el?.sprites.other.dream_world.front_default}
+              />
+              <Card.Body>
+                <Card.Title>{el?.name?.toUpperCase()}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </Card.Group> */}
-    </>
+      </Row>
+    </Container>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "semantic-ui-css/semantic.min.css";
 import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
